9cc5a1862b9dc0a651b4006228ff02be252216b5
AlexBCX <alexxbc1998@gmail.com>
2024-12-08 15:46:24 +0100

    push de todo

--- Practica5_2024/src/greedy/SolucióVoraç.java
+++ Practica5_2024/src/greedy/SolucióVoraç.java
@@ -25,31 +25,22 @@ public class SolucióVoraç {
     }
 
     public char[][] getSolucio() {
-        return solucio;
+        return this.solucio;
     }
 
-    private void greedy() {
+    private char[][] greedy() {
+        int paraulesAnotades = 0;
         for (PosicioInicial pos : repte.getEspaisDisponibles()) {
-            int mejorIndice = -1;
-            int mejorValor = -1;
-
             // Intentamos encontrar la mejor palabra para la posición actual
             for (int i = 0; i < repte.getItemsSize(); i++) {
                 if (!marcatge[i] && esAcceptable(pos, repte.getItem(i))) {
-                    int valorActual = calcularValor(pos, repte.getItem(i));
-                    if (valorActual > mejorValor) {
-                        mejorValor = valorActual;
-                        mejorIndice = i;
-                    }
+                    anotarASolucio(this.repte.getEspaisDisponibles().get(i), this.repte.getItem(i));
+                    paraulesAnotades++;
+                    if (this.repte.getEspaisDisponibles().size() == paraulesAnotades) return this.solucio;
                 }
             }
-
-            // Si encontramos una palabra adecuada, la colocamos
-            if (mejorIndice != -1) {
-                anotarASolucio(pos, repte.getItem(mejorIndice));
-                marcatge[mejorIndice] = true;
-            }
         }
+        return this.solucio;
     }
 
     private boolean esAcceptable(PosicioInicial pos, char[] item) {
@@ -72,25 +63,6 @@ public class SolucióVoraç {
             }
         }
         return true;
-    }
-
-    private int calcularValor(PosicioInicial pos, char[] item) {
-        int puntuacio = 0;
-        int fila = pos.getInitRow();
-        int columna = pos.getInitCol();
-
-        for (int i = 0; i < item.length; i++) {
-            char c;
-            if (pos.getDireccio() == 'H') {
-                c = solucio[fila][columna + i];
-            } else {
-                c = solucio[fila + i][columna];
-            }
-            if (c == item[i]) {
-                puntuacio++;
-            }
-        }
-        return puntuacio;
     }
 
     private void anotarASolucio(PosicioInicial pos, char[] item) {
